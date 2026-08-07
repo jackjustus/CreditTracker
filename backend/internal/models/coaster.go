8e680f1cee1758207cc32bb914de17f63974da91
Jack Justus <58899065+jackjustus@users.noreply.github.com>
2026-08-07 03:46:04 -0400

    restructure & refactor (#2)

--- backend/internal/models/coaster.go
+++ backend/internal/models/coaster.go
@@ -1,31 +1,30 @@
-package views
+package models
 
 import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/jackjustus/credittracker/backend/internal/gen/db"
 	api "github.com/jackjustus/credittracker/backend/internal/gen/openapi"
 )
 
 type Coaster struct {
-	ID   uuid.UUID
-	Name string
+	id   uuid.UUID
+	name string
 }
 
 func (c *Coaster) ToRide(time time.Time) api.Ride {
 	return api.Ride{
 		Coaster: api.Coaster{
-			Id:   c.ID,
-			Name: c.Name,
+			Id:   c.id,
+			Name: c.name,
 		},
 		RiddenAt: time,
 	}
 }
 
-func NewCoaster(row db.HydrateCoasterRow) *Coaster {
+func NewCoaster(id uuid.UUID, name string) *Coaster {
 	return &Coaster{
-		row.Coaster.ID,
-		row.Coaster.Name,
+		id,
+		name,
 	}
 }
