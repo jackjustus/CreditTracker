abfc2a47c91fac287f2840c9b6dc67e62120a9ab
jackjustus <jackjustushome@gmail.com>
2026-08-12 16:15:27 -0700

    fix func name

--- backend/internal/handler/coasters.go
+++ backend/internal/handler/coasters.go
@@ -6,6 +6,6 @@ import (
 	api "github.com/jackjustus/credittracker/backend/internal/gen/openapi"
 )
 
-func (s Server) GetCoaster(ctx context.Context, req api.SearchCoasterRequestObject) (api.SearchCoasterResponseObject, error) {
+func (s Server) SearchCoaster(ctx context.Context, req api.SearchCoasterRequestObject) (api.SearchCoasterResponseObject, error) {
 	return api.SearchCoaster200JSONResponse{}, nil
 }
